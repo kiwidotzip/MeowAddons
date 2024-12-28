055f5ff41a3cd76a86d5cc9584b72ee237c55090
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2024-12-28 18:43:09 +0530

    Fix improper chat message detection in guild chat

--- features/guildformat.js
+++ features/guildformat.js
@@ -4,5 +4,6 @@ register("chat", (hypixelrank, username, guildrank, message, event) => {
     if (!Settings.guildformat) return;
     cancel(event);
     const formattedGuildRank = guildrank ? `&8${guildrank} ` : "";
+
     ChatLib.chat(`&2G &8> ${formattedGuildRank}&a${username}&f: ${message}`);
-}).setCriteria(/Guild > (\[.+\])? ?(.+?) ?(\[.+?\])?: (.+)/);
+}).setCriteria(/Guild > ?(\[.+?\])? ?(.+?) ?(\[.+?\])?: (.+)/);

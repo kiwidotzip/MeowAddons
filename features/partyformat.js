978dd5f8c12112a2158badad488505ba5363ab3f
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-02 19:50:07 +0530

    Update partyformat.js to account for custom color and rank color

--- features/partyformat.js
+++ features/partyformat.js
@@ -1,7 +1,22 @@
 import Settings from "../config";
 
+
 register("chat", (hypixelrank, username, message, event) => {
     if (!Settings.partyformat) return;
     cancel(event);
-    ChatLib.chat(`&9P &8> &b${username}&f: ${message}`);
+    const rankColors = {
+        "Admin": "&c",
+        "Mod": "&2",
+        "Helper": "&9",
+        "GM": "&2",
+        "MVP++": `&${Settings.mvppluspluscolor}`,
+        "MVP+": `&${Settings.mvppluscolor}`,
+        "MVP": `&${Settings.mvpcolor}`,
+        "VIP+": `&${Settings.vippluscolor}`,
+        "VIP": `&${Settings.vipcolor}`,
+    };
+    const rankNameMatch = hypixelrank ? hypixelrank.match(/\[(.+?)\]/) : null;
+    const rankName = rankNameMatch ? rankNameMatch[1] : null;
+    const rankColor = rankName && rankColors[rankName] ? rankColors[rankName] : "&7";
+    ChatLib.chat(`&9P &8> ${rankColor}${username}&f: ${message}`);
 }).setCriteria(/Party > (\[.+?\])? ?(.+?): (.+)/);

4c0d0084292e1fcb2f6e88e565401729751dc9d6
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-02 16:02:35 +0530

    Clean up blood.js and move indungeon to helperfunction.js

--- features/blood.js
+++ features/blood.js
@@ -1,8 +1,8 @@
 import Settings from "../config";
+import { InDungeon } from "./helperfunction";
 
 let bloodopen = false
-let indungeon = false
-let starttime= 0
+let starttime = 0
 let speaktime = 0
 let diftime = 0
 let spawnalltime = 0
@@ -11,7 +11,6 @@ var blooddone = false
 
 register ("worldLoad", () => {
     bloodopen = false
-    indungeon = false
     starttime = 0
     speaktime = 0
     diftime = 0
@@ -19,27 +18,17 @@ register ("worldLoad", () => {
     camptime = 0
     blooddone = false
 })
-register ("worldLoad", () => {
-    if (!Settings.debug) return;
-    Client.scheduleTask(150, () => {
-        ChatLib.chat(`&dMeowAddons &8»&r InDungeon: ${indungeon}`);
-    })
-})
+
 register("chat", (event) => {
     if (!Settings.blood) return;
-//DUNGEON CHECK
-    if (TabList.getNames()?.includes("§r       §r§3§lDungeon Stats§r")) {
-        indungeon = true;
-    }
-    if (!indungeon) return; {
-        const message = ChatLib.getChatMessage(event);
+    if (!InDungeon) return;
 //BLOOD OPEN
-        if (message.startsWith("[BOSS] The Watcher:") && !bloodopen) {
+        if (ChatLib.getChatMessage(event).startsWith("[BOSS] The Watcher:") && !bloodopen) {
             bloodopen = true;
             starttime = Date.now();
         }
 //WATCHER DIALOGUE
-        if (message.startsWith("[BOSS] The Watcher: Let's see how you can handle this.")) {
+        if (ChatLib.getChatMessage(event).startsWith("[BOSS] The Watcher: Let's see how you can handle this.")) {
             speaktime = Date.now();
             diftime = (speaktime - starttime) / 1000;
 //FAST WATCHER
@@ -69,7 +58,7 @@ register("chat", (event) => {
             }
         }
 //SPAWNED ALL MOBS
-        if (message.startsWith("[BOSS] The Watcher: That will be enough for now.")) {
+        if (ChatLib.getChatMessage(event).startsWith("[BOSS] The Watcher: That will be enough for now.")) {
             spawnalltime = ((Date.now() - starttime) / 1000).toFixed(1);
             Client.showTitle(`&c!`, `&cWatcher finished spawning mobs!`, 2, 45, 10);
             ChatLib.chat(`&dMeowAddons &8» &rWatcher took &e${spawnalltime}s&r to spawn all mobs!`);
@@ -78,7 +67,7 @@ register("chat", (event) => {
             }
         }
 //CAMP TIME
-        if (message.startsWith("[BOSS] The Watcher: You have proven yourself. You may pass.") && starttime != 0) {
+        if (ChatLib.getChatMessage(event).startsWith("[BOSS] The Watcher: You have proven yourself. You may pass.") && starttime != 0) {
             blooddone = true;
             camptime = ((Date.now() - starttime) / 1000).toFixed(1);
             ChatLib.chat(`&dMeowAddons &8» &rBlood camp took &e${camptime}s&r!`);
@@ -86,5 +75,13 @@ register("chat", (event) => {
                 ChatLib.command(`pc MeowAddons » Blood camp took ${camptime}s!`);
             }
         }
-    }
+    });
+//DEBUG
+register("worldLoad", () => {
+    Client.scheduleTask(150, () => {
+        if (Settings.debug) {
+            const indungeondebug = InDungeon();
+            ChatLib.chat(`&c&lMeowAddons-DEBUG &8» &rInDungeon: &c${indungeondebug}`);
+        }
+    });
 });

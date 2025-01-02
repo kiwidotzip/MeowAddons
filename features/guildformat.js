397a584a517ef22819683da169bd7c8e963cd819
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-02 19:50:30 +0530

    Update guildformat.js to account for custom color and rank color

--- features/guildformat.js
+++ features/guildformat.js
@@ -3,7 +3,20 @@ import Settings from "../config";
 register("chat", (hypixelrank, username, guildrank, message, event) => {
     if (!Settings.guildformat) return;
     cancel(event);
-    const formattedGuildRank = guildrank ? `&8${guildrank} ` : "";
-
-    ChatLib.chat(`&2G &8> ${formattedGuildRank}&a${username}&f: ${message}`);
+    const rankColors = {
+        "Admin": "&c",
+        "Mod": "&2",
+        "Helper": "&9",
+        "GM": "&2",
+        "MVP++": `&${Settings.mvppluspluscolor}`,
+        "MVP+": `&${Settings.mvppluscolor}`,
+        "MVP": `&${Settings.mvpcolor}`,
+        "VIP+": `&${Settings.vippluscolor}`,
+        "VIP": `&${Settings.vipcolor}`,
+    };
+    const rankNameMatch = hypixelrank ? hypixelrank.match(/\[(.+?)\]/) : null;
+    const rankName = rankNameMatch ? rankNameMatch[1] : null;
+    const rankColor = rankName && rankColors[rankName] ? rankColors[rankName] : "&7";
+    const formattedGuildRank = guildrank ? `&8${guildrank}` : "";
+    ChatLib.chat(`&2G &8> ${formattedGuildRank} ${rankColor}${username}&f: ${message}`);
 }).setCriteria(/Guild > ?(\[.+?\])? ?(.+?) ?(\[.+?\])?: (.+)/);

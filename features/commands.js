0f90c1522f338bb3c9412d0763d444699db96649
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-03 18:15:55 +0530

    Change from Vigilance to Amaterasu - delete commands.js

--- features/commands.js
+++
@@ -1,8 +0,0 @@
-import Settings from "../config"
-
-register("command", () =>!Settings.openGUI())
-                                  .setName("meowaddons")
-                                  .setAliases("ma", "meowa");
-register("command", () => {
-    ChatLib.chat(`&00 &11 &22 &33 &44 &55 &66 &77 &88 &99 &aa &bb &cc &dd &ee &ff`)
-}).setName("colorr");

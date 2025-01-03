58e67e36ee861dd878f95213b26bc575cd2f7019
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-03 13:13:36 +0530

    Update cleanguildjoin.js (forgor to update)

--- features/cleanguildjoin.js
+++ features/cleanguildjoin.js
@@ -1,8 +1,8 @@
 import Settings from "../config";
 
-register("chat", (username,joinleftmsg,event) => {
+register("chat", (color,username,joinleftmsg,event) => {
     if (!Settings.cleanguildjoin) return
         cancel(event)
         if (joinleftmsg == "joined") ChatLib.chat(`&8G &2>> &a${username}`)
             if (joinleftmsg == "left") ChatLib.chat(`&8G &4<< &a${username}`)
-}).setCriteria(/Guild > (.+) (.+)\./)
+}).setCriteria(/&2Guild > &r&(a|b|6|c|d|2|9|7)(.+) &r&e(.+)\.&r/)

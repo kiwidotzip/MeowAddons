c350eeee1e2a72b7b957140a10be222bdd3e9db9
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-03 18:06:16 +0530

    Change from Vigilance to Amaterasu - update config.js

--- config.js
+++ config.js
@@ -1,318 +1,296 @@
-import {
-    @Vigilant,
-    @TextProperty,
-    @ButtonProperty,
-    @SwitchProperty,
-    @ColorProperty,
-    @DecimalSliderProperty,
-    Color,
-    @SelectorProperty
-} from "../Vigilance";
+// Make sure these go to the right directory 
+import Settings from "../Amaterasu/core/Settings"
+import DefaultConfig from "../Amaterasu/core/DefaultConfig"
+const defaultConf = new DefaultConfig("MeowAddons", "data/settings.json")
 
-@Vigilant("MeowAddons", "Meow Addons", {
-    getCategoryComparator: () => (a, b) => {
-        const categories = ["Chat", "Dungeons", "Fishing", "Party Commands", "Credits"];
-        return 1
-    }
+// Chat - General
+
+.addSwitch({
+    category: "Chat",
+    configName: "chatcleaner",
+    title: "Chat cleaner",
+    description: "Hides a LOT of useless messages",
+    subcategory: "General"
+})
+.addSwitch({
+    category: "Chat",
+    configName: "betterah",
+    title: "BetterAH",
+    description: "Formats Auction House messages\n&4Currently somewhat broken for CO-OP AH",
+    subcategory: "General"
+})
+.addSwitch({
+    category: "Chat",
+    configName: "betterbz",
+    title: "BetterBZ",
+    description: "Formats Bazaar messages",
+    subcategory: "General"
 })
 
-class Config {
+// Chat - Join/Leave format
 
-    // Chat - General
-    @SwitchProperty({
-        name: "Chat cleaner",
-        description: "Hides a LOT of useless messages",
-        category: "Chat",
-        subcategory: "1 - General"
-    })
-    chatcleaner = false
+.addSwitch({
+    category: "Chat",
+    configName: "cleantoggle",
+    title: "Clean join/leave messages",
+    description: "Formats join/leave messages in a clean way",
+    subcategory: "Join/Leave format"
+})
+.addSwitch({
+    category: "Chat",
+    configName: "cleanfriendjoin",
+    title: "Clean friend join messages",
+    description: "Formats friend join messages",
+    subcategory: "Join/Leave format",
+    shouldShow: data => data.cleantoggle
+})
+.addSwitch({
+    category: "Chat",
+    configName: "cleanguildjoin",
+    title: "Clean guild join messages",
+    description: "Formats guild join messages",
+    subcategory: "Join/Leave format",
+    shouldShow: data => data.cleantoggle
+})
+.addSwitch({
+    category: "Chat",
+    configName: "cleanpartyjoin",
+    title: "Clean party join messages",
+    description: "Formats party join messages",
+    subcategory: "Join/Leave format",
+    shouldShow: data => data.cleantoggle
+})
 
-    @SwitchProperty({
-        name: "BetterAH",
-        description: "Formats Auction House messages\n&4Currently somewhat broken for CO-OP AH",
-        category: "Chat",
-        subcategory: "1 - General"
-    })
-    betterah = false
+// Chat - Chat format
 
-    @SwitchProperty({
-        name: "BetterBZ",
-        description: "Formats Bazaar messages",
-        category: "Chat",
-        subcategory: "1 - General"
-    })
-    betterbz = false
+.addSwitch({
+    category: "Chat",
+    configName: "formatchatmessage",
+    title: "Toggle chat formatting",
+    description: "Formats chat messages in a clean way",
+    subcategory: "Chat format",
+})
+.addSwitch({
+    category: "Chat",
+    configName: "partyformat",
+    title: "Format party messages",
+    description: "Formats party messages",
+    subcategory: "Chat format",
+    shouldShow: data => data.formatchatmessage,
+})
+.addSwitch({
+    category: "Chat",
+    configName: "guildformat",
+    title: "Format guild messages",
+    description: "Formats guild messages",
+    subcategory: "Chat format",
+    shouldShow: data => data.formatchatmessage,
+})
 
-    // Chat - Join/Leave format
-    @SwitchProperty({
-        name: "Clean join/leave messages",
-        description: "Formats join/leave messages in a clean way",
-        category: "Chat",
-        subcategory: "2 - Join/Leave format"
-    })
-    cleantoggle = false
+// Chat - Custom chat rank (username) color
 
-    @SwitchProperty({
-        name: "Clean friend join messages",
-        description: "Formats friend join messages",
-        category: "Chat",
-        subcategory: "2 - Join/Leave format"
-    })
-    cleanfriendjoin = false
+.addSwitch({
+    category: "Chat",
+    configName: "togglecustomchatrankcolor",
+    title: "Toggle custom chat rank color",
+    description: "Toggles custom chat rank color\n&4Requires party chat and guild chat formatting to be enabled",
+    subcategory: "Custom chat rank color",
+})
+.addTextInput({
+    category: "Chat",
+    configName: "mvppluspluscolor",
+    title: "Color code for MVP++ rank",
+    description: "Color code for MVP++ rank",
+    value: "6",
+    placeHolder: "6",
+    subcategory: "Custom chat rank color",
+    shouldShow: data => data.togglecustomchatrankcolor,
+})
+.addTextInput({
+    category: "Chat",
+    configName: "mvppluscolor",
+    title: "Color code for MVP+ rank",
+    description: "Color code for MVP+ rank",
+    value: "b",
+    placeHolder: "b",
+    subcategory: "Custom chat rank color",
+    shouldShow: data => data.togglecustomchatrankcolor,
+})
+.addTextInput({
+    category: "Chat",
+    configName: "mvpcolor",
+    title: "Color code for MVP rank",
+    description: "Color code for MVP rank",
+    value: "b",
+    placeHolder: "b",
+    subcategory: "Custom chat rank color",
+    shouldShow: data => data.togglecustomchatrankcolor
+})
+.addTextInput({
+    category: "Chat",
+    configName: "vippluscolor",
+    title: "Color code for VIP+ rank",
+    description: "Color code for VIP+ rank",
+    value: "a",
+    placeHolder: "a",
+    subcategory: "Custom chat rank color",
+    shouldShow: data => data.togglecustomchatrankcolor
+})
+.addTextInput({
+    category: "Chat",
+    configName: "vipcolor",
+    title: "Color code for VIP rank",
+    description: "Color code for VIP rank",
+    value: "a",
+    placeHolder: "a",
+    subcategory: "Custom chat rank color",
+    shouldShow: data => data.togglecustomchatrankcolor
+})
 
-    @SwitchProperty({
-        name: "Clean guild join messages",
-        description: "Formats guild join messages",
-        category: "Chat",
-        subcategory: "2 - Join/Leave format"
-    })
-    cleanguildjoin = false
+// Chat - UAYOR
 
-    @SwitchProperty({
-        name: "Clean party join messages",
-        description: "Formats party join messages",
-        category: "Chat",
-        subcategory: "2 - Join/Leave format"
-    })
-    cleanpartyjoin = false
+.addSwitch({
+    category: "Chat",
+    configName: "automeow",
+    title: "Automeow",
+    description: "Sends \"meow\" when someone says \"meow :3\" in chat.",
+    subcategory: "UAYOR"
+})
 
-    // Chat - Chat format
-    @SwitchProperty({
-        name: "Toggle chat formatting",
-        description: "Formats chat messages in a clean way",
-        category: "Chat",
-        subcategory: "3 - Chat format"
-    })
-    formatchatmessage = false
+// Dungeons - Mask notifications
 
-    @SwitchProperty({
-        name: "Format party messages",
-        description: "Formats party messages",
-        category: "Chat",
-        subcategory: "3 - Chat format"
-    })
-    partyformat = false
+.addSwitch({
+    category: "Dungeons",
+    configName: "masknotifier",
+    title: "Mask Notifications",
+    description: "Notifies party chat when a mask pops",
+    subcategory: "Mask notifications"
+})
 
-    @SwitchProperty({
-        name: "Format guild messages",
-        description: "Formats guild messages",
-        category: "Chat",
-        subcategory: "3 - Chat format"
-    })
-    guildformat = false
+// ungeons - Blood helper
 
-    // Chat - Custom chat rank color
-    @SwitchProperty({
-        name: "Toggle custom chat rank color",
-        description: "Toggles custom chat rank color\n&4Requires party chat and guild chat formatting to be enabled",
-        category: "Chat",
-        subcategory: "4 - Custom chat rank color"
-    })
-    togglecustomchatrankcolor = false
+.addSwitch({
+    category: "Dungeons",
+    configName: "blood",
+    title: "Blood Helper",
+    description: "Main toggle for blood helper",
+    subcategory: "Blood helper"
+})
+.addSwitch({
+    category: "Dungeons",
+    configName: "sendbloodparty",
+    title: "Send blood information",
+    description: "Sends blood times to party chat",
+    subcategory: "Blood helper",
+    shouldShow: data => data.blood
+})
 
-    @TextProperty({
-        name: "Color code for MVP++ rank",
-        description: "Color code for MVP++ rank",
-        category: "Chat",
-        subcategory: "4 - Custom chat rank color",
-        placeholder: "6"
-    })
-    mvppluspluscolor = "6"
+// Fishing - Fishing messagees
 
-    @TextProperty({
-        name: "Color code for MVP+ rank",
-        description: "Color code for MVP+ rank",
-        category: "Chat",
-        subcategory: "4 - Custom chat rank color",
-        placeholder: "b"
-    })
-    mvppluscolor = "b"
+.addSwitch({
+    category: "Fishing",
+    configName: "fishingmsgmaintoggle",
+    title: "Main toggle",
+    description: "Main toggle for fishing messages",
+    subcategory: "Fishing messages"
+})
+.addSwitch({
+    category: "Fishing",
+    configName: "fishingmsg",
+    title: "Fishing messages",
+    description: "Notifies party chat when a fishing boss spawns",
+    subcategory: "Fishing messages",
+    shouldShow: data => data.fishingmsgmaintoggle
+})
+.addSwitch({
+    category: "Fishing",
+    configName: "fishingmsgsilly",
+    title: "Silly Fishing messages",
+    description: "Sends more lively messages when a fishing boss spawns",
+    subcategory: "Fishing messages",
+    shouldShow: data => data.fishingmsgmaintoggle
+})
 
-    @TextProperty({
-        name: "Color code for MVP rank",
-        description: "Color code for MVP rank",
-        category: "Chat",
-        subcategory: "4 - Custom chat rank color",
-        placeholder: "b"
-    })
-    mvpcolor = "b"
+// Party commands
 
-    @TextProperty({
-        name: "Color code for VIP+ rank",
-        description: "Color code for VIP+ rank",
-        category: "Chat",
-        subcategory: "4 - Custom chat rank color",
-        placeholder: "a"
-    })
-    vippluscolor = "a"
+.addSwitch({
+    category: "Party Commands",
+    configName: "partycommands",
+    title: "Party Commands",
+    description: "Enables party commands",
+    subcategory: "Main toggle"
+})
+.addSwitch({
+    category: "Party Commands",
+    configName: "partytransfer",
+    title: "Party Transfer",
+    description: "Transfers the party to the player that ran !ptme",
+    subcategory: "Sub Toggle",
+    shouldShow: data => data.partycommands,
+})
+.addSwitch({
+    category: "Party Commands",
+    configName: "partywarp",
+    title: "Party Warp",
+    description: "Warps the party when someone says !warp",
+    subcategory: "Sub Toggle",
+    shouldShow: data => data.partycommands,
+})
+.addSwitch({
+    category: "Party Commands",
+    configName: "partyinvite",
+    title: "Party Invite",
+    description: "Runs the party invite command when someone says !inv, !invite, !party, !p username in chat",
+    subcategory: "Sub Toggle",
+    shouldShow: data => data.partycommands,
+})
+.addSwitch({
+    category: "Party Commands",
+    configName: "partyallinvite",
+    title: "AllInvite",
+    description: "Toggles allinvite when someone says !allinv or !allinvite",
+    subcategory: "Sub Toggle",
+    shouldShow: data => data.partycommands,
+})
+.addSwitch({
+    category: "Party Commands",
+    configName: "partykickoffline",
+    title: "KickOffline",
+    description: "Kicks all offline players from the party when someone says !kickoffline",
+    subcategory: "Sub Toggle",
+    shouldShow: data => data.partycommands,
+})
 
-    @TextProperty({
-        name: "Color code for VIP rank",
-        description: "Color code for VIP rank",
-        category: "Chat",
-        subcategory: "4 - Custom chat rank color",
-        placeholder: "a"
-    })
-    vipcolor = "a"
+// Dev
 
-    // Chat - UAYOR
-    @SwitchProperty({
-        name: "Automeow",
-        description: "Sends \"meow\" when someone says \"meow :3\" in chat.",
-        category: "Chat",
-        subcategory: "5 - UAYOR"
-    })
-    automeow = false
-
-    // Dungeons - Mask notifications
-    @SwitchProperty({
-        name: "Mask Notifications",
-        description: "Notifies party chat when a mask pops",
-        category: "Dungeons",
-        subcategory: "1 - Mask notifications"
-    })
-    masknotifier = false
-
-    // Dungeons - Blood helper
-    @SwitchProperty({
-        name: "Blood Helper",
-        description: "Main toggle for blood helper",
-        category: "Dungeons",
-        subcategory: "2 - Blood helper"
-    })
-    blood = false
-
-    @SwitchProperty({
-        name: "Send blood information",
-        description: "Sends blood times to party chat",
-        category: "Dungeons",
-        subcategory: "2 - Blood helper"
-    })
-    sendbloodparty = false
-
-    // Fishing - Fishing messages
-    @SwitchProperty({
-        name: "Main toggle",
-        description: "Main toggle for fishing messages",
-        category: "Fishing",
-        subcategory: "1 - Fishing messages"
-    })
-    fishingmsgmaintoggle = false
-
-    @SwitchProperty({
-        name: "Fishing messages",
-        description: "Notifies party chat when a fishing boss spawns",
-        category: "Fishing",
-        subcategory: "1 - Fishing messages"
-    })
-    fishingmsg = false
-
-    @SwitchProperty({
-        name: "Silly Fishing messages",
-        description: "Sends more lively messages when a fishing boss spawns",
-        category: "Fishing",
-        subcategory: "1 - Fishing messages"
-    })
-    fishingmsgsilly = false
-
-    // Party Commands - Main toggle
-    @SwitchProperty({
-        name: "Party Commands",
-        description: "Enables party commands",
-        category: "Party Commands",
-        subcategory: "1 - Main toggle"
-    })
-    partycommands = false
-
-    // Party Commands - Sub Toggle
-    @SwitchProperty({
-        name: "Party Transfer",
-        description: "Transfers the party to the player that ran !ptme",
-        category: "Party Commands",
-        subcategory: "2 - Sub Toggle"
-    })
-    partytransfer = false
-
-    @SwitchProperty({
-        name: "Party Warp",
-        description: "Warps the party when someone says !warp",
-        category: "Party Commands",
-        subcategory: "2 - Sub Toggle"
-    })
-    partywarp = false
-
-    @SwitchProperty({
-        name: "Party Invite",
-        description: "Runs the party invite command when someone says !inv, !invite, !party, !p username in chat",
-        category: "Party Commands",
-        subcategory: "2 - Sub Toggle"
-    })
-    partyinvite = false
-
-    @SwitchProperty({
-        name: "AllInvite",
-        description: "Toggles allinvite when someone says !allinv or !allinvite",
-        category: "Party Commands",
-        subcategory: "2 - Sub Toggle"
-    })
-    partyallinvite = false
-
-    @SwitchProperty({
-        name: "KickOffline",
-        description: "Kicks all offline players from the party when someone says !kickoffline",
-        category: "Party Commands",
-        subcategory: "2 - Sub Toggle"
-    })
-    partykickoffline = false
-
-    // Credits - Developer
-    @ButtonProperty({
-        name: "Kiwidotzip",
-        description: "Developer",
-        category: "Credits",
-        subcategory: "1 - Developer",
-        placeholder: " "
-    })
-    credit1() {};
-
-    @ButtonProperty({
-        name: "Sascha_Vadkovson (Scatha)",
-        description: "Developer",
-        category: "Credits",
-        subcategory: "1 - Developer",
-        placeholder: " "
-    })
-    credit2() {};
-
-    // Credits - Debug
-    @SwitchProperty({
-        name: "Debug",
-        description: "Enables debug messages",
-        category: "Credits",
-        subcategory: "2 - Debug"
-    })
-    debug = false
-
-    constructor() {
-        this.initialize(this)
-        this.addDependency("AllInvite", "Party Commands")
-        this.addDependency("KickOffline", "Party Commands")
-        this.addDependency("Party Transfer", "Party Commands")
-        this.addDependency("Party Warp", "Party Commands")
-        this.addDependency("Party Invite", "Party Commands")
-        this.addDependency("Clean friend join messages", "Clean join/leave messages")
-        this.addDependency("Clean guild join messages", "Clean join/leave messages")
-        this.addDependency("Clean party join messages", "Clean join/leave messages")
-        this.addDependency("Format party messages", "Toggle chat formatting")
-        this.addDependency("Format guild messages", "Toggle chat formatting")
-        this.addDependency("Fishing messages", "Main toggle")
-        this.addDependency("Silly Fishing messages", "Main toggle")
-        this.addDependency("Send blood information", "Blood Helper")
-        this.addDependency("Color code for MVP++ rank", "Toggle custom chat rank color")
-        this.addDependency("Color code for MVP+ rank", "Toggle custom chat rank color")
-        this.addDependency("Color code for MVP rank", "Toggle custom chat rank color")
-        this.addDependency("Color code for VIP+ rank", "Toggle custom chat rank color")
-        this.addDependency("Color code for VIP rank", "Toggle custom chat rank color")
+.addButton({
+    category: "Developer",
+    configName: "credit1",
+    title: "Kiwidotzip",
+    description: "Developer",
+    subcategory: "Developer",
+    onClick() {
     }
-}
-export default new Config()
+})
+.addButton({
+    category: "Developer",
+    configName: "credit2",
+    title: "Sascha_Vadkovson (Scatha)",
+    description: "Developer",
+    subcategory: "Developer",
+    onClick() {
+    }
+})
+.addSwitch({
+    category: "Developer",
+    configName: "debug",
+    title: "Debug",
+    description: "Enables debug messages",
+    subcategory: "Debug"
+})
+
+const config = new Settings("MeowAddons", defaultConf, "data/ColorScheme.json").setCommand("MeowAddons", ["ma", "meowa"])
+config
+      .setSize(60,60)
+      .apply()
+export default () => config.settings

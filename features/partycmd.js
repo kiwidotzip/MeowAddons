5e15f716a0bf4dfff66ad8c6852121a536b266d4
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-03 18:20:10 +0530

    Change from Vigilance to Amaterasu - update partycmd.js

--- features/partycmd.js
+++ features/partycmd.js
@@ -1,37 +1,37 @@
-import Settings from "../config";
+import Config from "../config";
 import Party from "../../BloomCore/Party"
 
 register('chat', (rank, name) => {
     if (name === Player.getName()) return;
-	if (!Settings.partytransfer) return
+	if (!Config().partytransfer && !Config().partycommands) return
 	if (Party.leader == Player.getName()) {
 		ChatLib.command(`party transfer ${name}`)
 	}
 }).setCriteria(/Party > (?:\[([^\]]*?)\] )?(\w{1,16}): !ptme$/)
 
 register('chat', (rank, name) => {
-	if (!Settings.partywarp) return
+	if (!Config().partywarp && !Config().partycommands) return
 	if (Party.leader == Player.getName()) {
 		ChatLib.command(`party warp`)
 	}
 }).setCriteria(/Party > (?:\[([^\]]*?)\] )?(\w{1,16}): !warp$/)
 
 register('chat', (rank, name) => {
-	if (!Settings.partyallinvite) return
+	if (!Config().partyallinvite && !Config().partycommands) return
 	if (Party.leader == Player.getName()) {
-		ChatLib.command(`party settings allinvite`)
+		ChatLib.command(`party Config() allinvite`)
 	}
 }).setCriteria(/Party > (?:\[([^\]]*?)\] )?(\w{1,16}): !allinv$/)
 
 register('chat', (rank, name) => {
-	if (!Settings.partykickoffline) return
+	if (!Config().partykickoffline && !Config().partycommands) return
 	if (Party.leader == Player.getName()) {
 		ChatLib.command(`party kickoffline`)
 	}
 }).setCriteria(/Party > (?:\[([^\]]*?)\] )?(\w{1,16}): !kickoffline$/)
 
 register('chat', (rank, name, alias, player) => {
-	if (!Settings.partyinvite) return
+	if (!Config().partyinvite && !Config().partycommands) return
 	if (Party.leader == Player.getName()) {
 		ChatLib.command(`party invite ${player}`)
 	}

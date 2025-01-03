595b8afb11e1eabbdd883682e78c8b80ad24c0e4
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-03 13:13:56 +0530

    Update cleanfriendjoin.js (forgor to update)

--- features/cleanfriendjoin.js
+++ features/cleanfriendjoin.js
@@ -1,8 +1,8 @@
 import Settings from "../config";
 
-register("chat", (username,joinleftmsg,event) => {
+register("chat", (color,username,joinleftmsg,event) => {
     if (!Settings.cleanfriendjoin) return
         cancel(event)
         if (joinleftmsg == "joined") ChatLib.chat(`&8F &2>> &b${username}`)
-            if (joinleftmsg == "left") ChatLib.chat(`&8F &4<< &b${username}`)
-}).setCriteria(/Friend > (.+) (.+)\./)
+        if (joinleftmsg == "left") ChatLib.chat(`&8F &4<< &b${username}`)
+}).setCriteria(/&aFriend > &r(&a|&b|&6|&c|&d|&2|&9)(.+) &r&e(.+)\.&r/)

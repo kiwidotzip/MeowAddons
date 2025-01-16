0091638e7e0a6f270c1e3eba4c8f9cca81a2d314
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-16 14:19:06 +0530

    Update cleanfriendjoin.js

--- features/cleanfriendjoin.js
+++ features/cleanfriendjoin.js
@@ -6,4 +6,4 @@ register("chat", (color, username, joinleftmsg, event) => {
     cancel(event);
     if (joinleftmsg === "joined") ChatLib.chat(`&8F &2>> ${color}${username}`);
     if (joinleftmsg === "left") ChatLib.chat(`&8F &4<< ${color}${username}`);
-}).setCriteria(/&aFriend > &r(&a|&b|&6|&c|&d|&2|&9|&7)(.+) &r&e(.+)\.&r/);
+}).setCriteria(/^&aFriend > &r(&a|&b|&6|&c|&d|&2|&9|&7)(.+) &r&e(.+)\.&r/);

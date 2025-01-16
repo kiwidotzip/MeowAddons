8c07f43e42d81678a5a2ce62ce1213d0fba83aa1
kiwidotzip <105352407+kiwidotzip@users.noreply.github.com>
2025-01-16 14:19:17 +0530

    Update cleanguildjoin.js

--- features/cleanguildjoin.js
+++ features/cleanguildjoin.js
@@ -6,4 +6,4 @@ register("chat", (color,username,joinleftmsg,event) => {
         cancel(event)
         if (joinleftmsg == "joined") ChatLib.chat(`&8G &2>> ${color}${username}`)
             if (joinleftmsg == "left") ChatLib.chat(`&8G &4<< ${color}${username}`)
-}).setCriteria(/&2Guild > &r(&a|&b|&6|&c|&d|&2|&9|&7)(.+) &r&e(.+)\.&r/)
+}).setCriteria(/^&2Guild > &r(&a|&b|&6|&c|&d|&2|&9|&7)(.+) &r&e(.+)\.&r/)

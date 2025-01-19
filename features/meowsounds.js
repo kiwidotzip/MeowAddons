97b8cef7708c63aca71495066889e7a791154ab0
kiwidotzip <snowykiwii@gmail.com>
2025-01-19 12:25:06 +0530

    fix oopsie

--- features/meowsounds.js
+++ features/meowsounds.js
@@ -1,7 +1,7 @@
 import Config from "../config"
 
 register("chat", (message, event) => {
-    if (Config().meowsounds) return;
+    if (!Config().meowsounds) return;
         if (message.toLowerCase().includes("meow")) 
             World.playSound("mob.cat.meow", 1, 1);
     }).setCriteria(/(?:Guild|Party|Co-op|From|To)? ?(?:>)? ?(?:\[.+?\])? ?(?:[a-zA-Z0-9_]+) ?(?:\[.+?\])?: (.+)/)

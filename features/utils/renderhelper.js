fc8451f28f6ed3576f494ff2d4edcff441e0d0c5
kiwidotzip <snowykiwii@gmail.com>
2025-03-11 08:36:56 +0530

    add comments

--- features/utils/renderhelper.js
+++ features/utils/renderhelper.js
@@ -9,25 +9,25 @@ export default function drawEntityBox(x, y, z, width, height, red, green, blue, 
         x + width / 2, y + height, z + width / 2
     );
     
-    GlStateManager.func_179090_x();
-    GlStateManager.func_179147_l();
-    GlStateManager.func_179140_f();
-    GlStateManager.func_179118_c();
-    GlStateManager.func_179120_a(770, 771, 1, 0);
+    GlStateManager.func_179090_x(); // disableTexture2D
+    GlStateManager.func_179147_l(); // enableBlend
+    GlStateManager.func_179140_f(); // disableLighting
+    GlStateManager.func_179118_c(); // disableAlpha
+    GlStateManager.func_179120_a(770, 771, 1, 0); // tryBlendFuncSeparate
     
     GL11.glLineWidth(lineWidth);
     
-    if (phase) GlStateManager.func_179097_i();
+    if (phase) GlStateManager.func_179097_i(); // disableDepth
     
     RenderGlobal.func_181563_a(aabb, red, green, blue, alpha);
     
-    if (phase) GlStateManager.func_179126_j();
+    if (phase) GlStateManager.func_179126_j(); // enableDepth
     
-    GlStateManager.func_179084_k();
-    GlStateManager.func_179141_d();
-    GlStateManager.func_179098_w();
-    GlStateManager.func_179131_c(1, 1, 1, 1);
-    GlStateManager.func_179145_e();
+    GlStateManager.func_179084_k(); // disableBlend
+    GlStateManager.func_179141_d(); // enableAlpha
+    GlStateManager.func_179098_w(); // enableTexture2D
+    GlStateManager.func_179131_c(1, 1, 1, 1); // color
+    GlStateManager.func_179145_e(); // enableLighting
     
     GL11.glLineWidth(2);
 }

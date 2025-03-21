6407033b52d5ed3f68f7c8c0e457145cf0c009c6
kiwidotzip <snowykiwii@gmail.com>
2025-03-21 10:57:20 +0530

    mask notifiers refactor

--- features/masknotifiers.js
+++ features/masknotifiers.js
@@ -1,25 +1,14 @@
 import Config from "../config";
 
-register("chat", (event) => {
-    if (!Config().masknotifier) return
-        cancel(event)
-        ChatLib.command(`pc MeowAddons » Bonzo Mask activated (3s)`)
-}).setCriteria("Your ⚚ Bonzo's Mask saved your life!")
-
-register("chat", (event) => {
-    if (!Config().masknotifier) return
-        cancel(event)
-        ChatLib.command(`pc MeowAddons » Bonzo Mask activated (3s)`)
-}).setCriteria("Your Bonzo's Mask saved your life!")
-
-register("chat", (event) => {
-    if (!Config().masknotifier) return
-        cancel(event)
-        ChatLib.command(`pc MeowAddons » Spirit Mask activated (3s)`)
-}).setCriteria("Second Wind Activated! Your Spirit Mask saved your life!")
-
-register("chat", (event) => {
-    if (!Config().masknotifier) return
-        cancel(event)
-        ChatLib.command(`pc MeowAddons » Phoenix Pet activated (2-4s)`)
-}).setCriteria("Your Phoenix Pet saved you from certain death!")
+register("chat", (msg, event) => {
+    if (!Config().masknotifier) return;
+    const mask = [
+      [/Your (?:. )?Bonzo's Mask saved your life!/, "Bonzo Mask activated (3s)"],
+      [/^Second Wind Activated! Your Spirit Mask saved your life!$/, "Spirit Mask activated (3s)"],
+      [/^Your Phoenix Pet saved you from certain death!$/, "Phoenix Pet activated (2-4s)"]
+    ].find(([pattern]) => pattern.test(msg));
+  
+    if (!mask) return;
+    ChatLib.command(`pc MeowAddons » ${mask[1]}`);
+    cancel(event);
+}).setCriteria("${msg}");

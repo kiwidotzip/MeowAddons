673ac2958c8080d952f1fb8b75eb056a2ec4db94
kiwidotzip <snowykiwii@gmail.com>
2025-04-15 17:13:31 +0530

    Update automeow.js

--- features/automeow.js
+++ features/automeow.js
@@ -1,5 +1,6 @@
 import Config from "../config";
 import { meowc } from "./utils/pogdata";
+import { registerWhen } from "./utils/renderutils";
 
 const PREFIX = "&e[MeowAddons]";
 const ACHIEVEMENTS = [10, 25, 50, 100, 250, 500, 1000, 2000, 5000, 10000];

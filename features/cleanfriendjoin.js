731ce7d3392426907b76be21c1ef8c94092c5bfb
kiwidotzip <snowykiwii@gmail.com>
2025-04-15 17:24:01 +0530

    Update cleanfriendjoin.js

--- features/cleanfriendjoin.js
+++ features/cleanfriendjoin.js
@@ -1,9 +1,8 @@
 import Config from "../config";
+import { registerWhen } from "./utils/renderutils";
 
-register("chat", (color, username, joinleftmsg, event) => {
-    if (!Config().cleanfriendjoin) return;
-    if (!Config().cleantoggle) return;
+registerWhen(register("chat", (color, username, joinleftmsg, event) => {
     cancel(event);
     if (joinleftmsg === "joined") ChatLib.chat(`&8F &2>> ${color}${username}`);
     if (joinleftmsg === "left") ChatLib.chat(`&8F &4<< ${color}${username}`);
-}).setCriteria(/^&aFriend > &r(&a|&b|&6|&c|&d|&2|&9|&7)(.+) &r&e(.+)\.&r/);
+}).setCriteria(/^&aFriend > &r(&a|&b|&6|&c|&d|&2|&9|&7)(.+) &r&e(.+)\.&r/), () => Config().cleanfriendjoin && Config().cleantoggle)

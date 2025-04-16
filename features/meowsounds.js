3777b2322d86177e1f19c36baacc235607ba7bb7
kiwidotzip <snowykiwii@gmail.com>
2025-04-16 08:59:23 +0530

    Update meowsounds.js

--- features/meowsounds.js
+++ features/meowsounds.js
@@ -1,10 +1,10 @@
 import Config from "../config"
+import { registerWhen } from "./utils/renderutils";
 const sounds = ["mob.cat.meow", "mob.cat.purreow", "mob.cat.straymeow"]
 
-register("chat", (message, event) => {
-    if (!Config().meowsounds) return;
+registerWhen(register("chat", (message, event) => {
         if (message.toLowerCase().includes("meow")) {
             let random = Math.floor(Math.random() * sounds.length)
             World.playSound(sounds[random], 1, 1);
         }
-    }).setCriteria(/(?:Guild|Party|Co-op|From|To)? ?(?:>)? ?(?:\[.+?\])? ?(?:[a-zA-Z0-9_]+) ?(?:\[.+?\])?: (.+)/)
+    }).setCriteria(/(?:Guild|Party|Co-op|From|To)? ?(?:>)? ?(?:\[.+?\])? ?(?:[a-zA-Z0-9_]+) ?(?:\[.+?\])?: (.+)/), () => Config().meowsounds)

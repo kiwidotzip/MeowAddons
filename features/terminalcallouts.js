ebf7c9f17fce11465936a1b20a55052c8593e32e
kiwidotzip <snowykiwii@gmail.com>
2025-04-16 09:02:57 +0530

    Update terminalcallouts.js

--- features/terminalcallouts.js
+++ features/terminalcallouts.js
@@ -1,8 +1,9 @@
 import Config from "../config";
+import { registerWhen } from "./utils/renderutils";
 
-register("chat", () => {
-    if (Config().sendTermInChat != 0 && Config().sendTermInChat != 5) 
+registerWhen(register("chat", () => {
+    if (Config().sendTermInChat != 5) 
         ChatLib.command(`pc I will do ${parseInt(Config().sendTermInChat)}!`);
     if (Config().sendTermInChat == 5) 
         ChatLib.command("pc I will do devices!");
-}).setCriteria("[BOSS] Storm: I should have known that I stood no chance.")
+}).setCriteria("[BOSS] Storm: I should have known that I stood no chance."), () => Config().sendTermInChat != 0)

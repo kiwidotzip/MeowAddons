53118e7fe3401de304d76261881fd06f0209e9a1
kiwidotzip <snowykiwii@gmail.com>
2025-04-25 21:38:23 +0530

    remove ""

--- features/mimic.js
+++ features/mimic.js
@@ -9,7 +9,7 @@ MimicChest
         if (ent.tileEntity.func_145980_j() !== 1) return; // check for trapped chest
         GL11.glPushMatrix();
         GL11.glColor3f(1.0, 0.0, 0.0);
-    }, ["net.minecraft.tileentity.TileEntityChest"])
+    }, [net.minecraft.tileentity.TileEntityChest])
     .register("ma:postRenderTileEntity", (ent) => {
         if (ent.tileEntity.func_145980_j() !== 1) return; // check for trapped chest
         GL11.glPopMatrix();

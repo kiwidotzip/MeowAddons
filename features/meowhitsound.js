3a44f71459eb0143e3ca571388a089cc8890fc12
kiwidotzip <snowykiwii@gmail.com>
2025-05-05 18:43:03 +0530

    actually fix the console spam and not just eat the error

--- features/meowhitsound.js
+++ features/meowhitsound.js
@@ -4,6 +4,7 @@ import { FeatManager } from "./helperfunction";
 const meowhitsound = FeatManager.createFeature("meowhitsound");
 const ArmorStand = Java.type("net.minecraft.entity.item.EntityArmorStand");
 const sounds = ["mob.cat.meow", "mob.cat.purreow", "mob.cat.straymeow"]
+const EnumParticleTypes = net.minecraft.util.EnumParticleTypes // ily doc
 
 meowhitsound.register("entityDeath", (e) => {
     if (!e || e.distanceTo(Player.getPlayer()) > Config().meowhitradius || e.getEntity() instanceof ArmorStand) return;
@@ -11,8 +12,6 @@ meowhitsound.register("entityDeath", (e) => {
     playSFX()
     playParticles(e)
 })
-
-// Credit to Xynth 
 
 const playSFX = () => {
     for (var i = 0; i < 2; i++) {
@@ -24,22 +23,19 @@ const playSFX = () => {
 }
 
 const playParticles = (entity) => {
-    let x = entity.getX();
-    let y = entity.getY();
-    let z = entity.getZ();
-    try {
-        for (var i = 0; i < 10; i++) {
-            setTimeout(() => {
-                if (Math.random() <= 0.66) {
-                    World.particle.spawnParticle(
-                        "NOTE",
-                        x + Math.random() * 0.4, y + 0.1 + Math.random() * 1.7, z + Math.random() * 0.4,
-                        0, 0, 0
-                    ).setMaxAge(6 + Math.random() * 6).scale(0.66 + Math.random() * 0.2)
-                }
-            }, i*30) 
-        }
-    } catch (err) {
-        // eat error
+    const world = World.getWorld()
+    let x = entity.getX()
+    let y = entity.getY()
+    let z = entity.getZ()
+    
+    for (var i = 0; i < 10; i++) {
+        setTimeout(() => {
+            if (Math.random() > 0.66) return
+            world.func_175688_a(
+                EnumParticleTypes.NOTE,
+                x + Math.random() * 0.4, y + 0.1 + Math.random() * 1.7, z + Math.random() * 0.4,
+                0, 0, 0, 0
+            )
+        }, i * 30)
     }
 }

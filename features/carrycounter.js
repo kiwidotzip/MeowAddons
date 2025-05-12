86bd80f836b7071835ee5d2b4ea8b0e322d2a08c
kiwidotzip <snowykiwii@gmail.com>
2025-05-12 09:43:39 +0530

    fix yet anotehr thing

--- features/carrycounter.js
+++ features/carrycounter.js
@@ -928,7 +928,7 @@ register("command", (...args = []) => {
     const playerNames = World.getAllPlayers()
         .filter(player => player.getUUID().version() === 4)
         .map(player => player.getName()?.removeFormatting())
-    if (args.length === 2 && modcmd.includes(subcommand)) return playerNames.filter(name => name.startsWith(currentArg))
+    if (args.length === 2 && modcmd.includes(subcommand)) return playerNames.filter(name => name.toLowerCase().startsWith(currentArg))
     if (args.length === 1) return allcmd.filter(c => c.startsWith(currentArg))
     return []
 }).setName("carry").setAliases(["macarry"]);
@@ -1023,7 +1023,7 @@ register("command", (...args = []) => {
     const playerNames = World.getAllPlayers()
         .filter(player => player.getUUID().version() === 4)
         .map(player => player.getName()?.removeFormatting())
-    if (args.length === 2 && modcmd.includes(subcommand)) return playerNames.filter(name => name.startsWith(currentArg))
+    if (args.length === 2 && modcmd.includes(subcommand)) return playerNames.filter(name => name.toLowerCase().startsWith(currentArg))
     if (args.length === 1) return allcmd.filter(c => c.startsWith(currentArg))
     return []
 }).setName("dgcarry").setAliases(["madgcarry"]);

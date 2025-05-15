1eb62038c377bf14fd5ffcfbcc9818792da104aa
kiwidotzip <snowykiwii@gmail.com>
2025-05-15 18:02:58 +0530

    hehe typo

--- features/slayertimer.js
+++ features/slayertimer.js
@@ -75,7 +75,7 @@ slayerkilltimer
     })
     .register("chat", () => {
         const timeTaken = Date.now() - timestarted
-        const msg = new Message(new TextComponent(`&e[MeowAddons] &fYour killed you boss in &b${(timeTaken / 1000).toFixed(2)}s&7 | &b${serverticks / 20}s.`)
+        const msg = new Message(new TextComponent(`&e[MeowAddons] &fYour boss killed you in &b${(timeTaken / 1000).toFixed(2)}s&7 | &b${serverticks / 20}s.`)
                                 .setHoverValue(`&c${serverticks} ticks &f| &c${timeTaken} ms &7- May not be 100% accurate`))
         ChatLib.chat(msg)
         resetBossTracker()

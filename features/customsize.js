5d82850c8ae8d54f7bf3141cf5f6b7733096a6b3
kiwidotzip <snowykiwii@gmail.com>
2025-05-20 08:18:16 +0530

    not redefine settings values every frame

--- features/customsize.js
+++ features/customsize.js
@@ -1,18 +1,26 @@
-import Settings from "../config";
-import { FeatManager } from "./helperfunction";
-const customsize = FeatManager.createFeature("customsize");
+import Config from "../config"
+import { FeatManager } from "./helperfunction"
+const customsize = FeatManager.createFeature("customsize")
 
 // Credit to Cubed module - /ct import cubed
 
+let x = Config().customX
+let y = Config().customY
+let z = Config().customZ
+
 customsize
     .register("renderEntity", entity => {
-        Tessellator.pushMatrix();
-        const { customX: x, customY: y, customZ: z } = Settings();
+        Tessellator.pushMatrix()
         if (y < 0) {
             Tessellator.translate(0, -2 * y, 0)
             Tessellator.rotate(180, 1, 0, 0)
             Tessellator.rotate(2 * entity.getYaw() + 180, 0, 1, 0)
         }
-        Tessellator.scale(x, Math.abs(y), z);
+        Tessellator.scale(x, Math.abs(y), z)
     }, net.minecraft.client.entity.EntityPlayerSP)
     .register("postRenderEntity", () => Tessellator.popMatrix(), net.minecraft.client.entity.EntityPlayerSP)
+
+Config().getConfig()
+                    .registerListener("customX", (oldv, newv) => x = newv)
+                    .registerListener("customY", (oldv, newv) => y = newv)
+                    .registerListener("customZ", (oldv, newv) => z = newv)
